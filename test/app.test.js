90de32cc54a5d6732219dc51513ff7a22db1940f
joelcorey <joelcorey@fastmail.com>
2019-04-02 12:23:22 -0700

    genre test added

--- test/app.test.js
+++ test/app.test.js
@@ -8,4 +8,39 @@ describe('Express App', () => {
       .get('/apps')
       .expect(200);
   });
+  // working test:
+  // it('GET /apps?sort=Rating should return sorted by rating', () => {
+  //   return request(app)
+  //     .get('/apps')
+  //     .query({sort: 'Rating'})
+  //     .expect(200)
+  //     .expect('Content-Type', /json/)
+  //     .then(res => {
+  //       expect(res.body).to.be.an('array');
+  //       let i = 0;
+  //       let sorted = true;
+  //       while(sorted && i < res.body.length - 1) {
+  //         sorted = sorted && res.body[i].Rating >= res.body[i + 1].Rating;
+  //         i++;
+  //       }
+  //       expect(sorted).to.be.true;
+  //     })
+  // })
+  it('GET /apps?genre=Action should return action genre apps', () => {
+    return request(app)
+      .get('/apps')
+      .query({genre: 'Action'})
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        let i = 0;
+        let sorted = true;
+        while(sorted && i < res.body.length - 1) {
+          sorted = sorted && res.body[i].Genres.includes('Action') ;
+          i++;
+        }
+        expect(sorted).to.be.true;
+      })
+  })
 });
