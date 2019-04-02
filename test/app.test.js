64c650e7720ed86c524879d6d21495dbc593de06
David Bolin <david@bolin.at>
2019-04-02 12:31:45 -0700

    code cleaning

--- test/app.test.js
+++ test/app.test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const app = require('../app');
 const expect = require('chai').expect;
 const request = require('supertest');
@@ -9,23 +11,25 @@ describe('Express App', () => {
       .expect(200);
   });
   // working test:
-  // it('GET /apps?sort=Rating should return sorted by rating', () => {
-  //   return request(app)
-  //     .get('/apps')
-  //     .query({sort: 'Rating'})
-  //     .expect(200)
-  //     .expect('Content-Type', /json/)
-  //     .then(res => {
-  //       expect(res.body).to.be.an('array');
-  //       let i = 0;
-  //       let sorted = true;
-  //       while(sorted && i < res.body.length - 1) {
-  //         sorted = sorted && res.body[i].Rating >= res.body[i + 1].Rating;
-  //         i++;
-  //       }
-  //       expect(sorted).to.be.true;
-  //     })
-  // })
+  
+  it('GET /apps?sort=Rating should return sorted by rating', () => {
+    return request(app)
+      .get('/apps')
+      .query({sort: 'Rating'})
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        let i = 0;
+        let sorted = true;
+        while(sorted && i < res.body.length - 1) {
+          sorted = sorted && res.body[i].Rating >= res.body[i + 1].Rating;
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
+
   it('GET /apps?genre=Action should return action genre apps', () => {
     return request(app)
       .get('/apps')
@@ -41,6 +45,6 @@ describe('Express App', () => {
           i++;
         }
         expect(sorted).to.be.true;
-      })
-  })
+      });
+  });
 });
